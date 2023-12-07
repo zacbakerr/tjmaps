26ba0a16e1357721524340e5436384ffeba1da15
Zac Baker <zacattackbaker@gmail.com>
2023-12-06 21:29:20 -0500

    Added data to label!

---
+++ dataLabeling/labelStudio/Lib/site-packages/label_studio/tasks/mixins.py
@@ -0,0 +1,21 @@
+class TaskMixin:
+    def has_permission(self, user: 'User') -> bool:  # noqa: F821
+        """Called by Task#has_permission"""
+        return True
+
+    def _get_is_labeled_value(self) -> bool:
+        n = self.completed_annotations.count()
+        return n >= self.overlap
+
+    def update_is_labeled(self, *args, **kwargs) -> None:
+        self.is_labeled = self._get_is_labeled_value()
+
+    @classmethod
+    def post_process_bulk_update_stats(cls, tasks) -> None:
+        pass
+
+
+class AnnotationMixin:
+    def has_permission(self, user: 'User') -> bool:  # noqa: F821
+        """Called by Annotation#has_permission"""
+        return True
